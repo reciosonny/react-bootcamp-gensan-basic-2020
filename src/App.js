bbd7575aa6c054141b50e5c29598ebbe4225969b
Sonny Recio <reciosonny@gmail.com>
2020-02-29 13:45:29 +0800

    Controlled component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,38 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { txtInput: "" };
 
-function App() {
+    this.onClearTextInput = this.onClearTextInput.bind(this);
+    this.onChangeTextInput = this.onChangeTextInput.bind(this);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onClearTextInput() {
+    this.setState({ txtInput: "" });
+  }
+
+  onChangeTextInput(e) {
+    const { value } = e.target;
+
+    this.setState({ txtInput: value });
+  }
+
+  render() {
+    const { txtInput } = this.state;
+
+    return (
+      <div className="App">
+        <input type="text" value={txtInput} onChange={this.onChangeTextInput} />
+        <br />
+
+        <button onClick={this.onClearTextInput}>Clear text input value</button>
+      </div>
+    );
+  }
 }
 
 export default App;
